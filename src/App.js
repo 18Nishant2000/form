d1ac446db1311f57b3a96aa5df5d2f6d911c3086
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-10-30 19:45:58 +0530

    Added Css And Boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+// eslint-disable-next-line
+import Button from "react-bootstrap/Button"
+import Frost from "./FrostedGlass.css"
+import Form from "react-bootstrap/Form"
+import Container from 'react-bootstrap/Container'
+import Row from "react-bootstrap/Row"
+import Col from 'react-bootstrap/Col'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="Frost mt-4">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+</Button>
+        </Form>
+      </div>
+    </Container>
+
   );
 }
 
