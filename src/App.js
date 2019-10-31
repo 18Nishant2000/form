70c1317bd73d02e7bf9f1236807ff97713a0827c
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-10-31 20:24:41 +0530

    Some UI changes And More Fields

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import Row from "react-bootstrap/Row"
 import Col from 'react-bootstrap/Col'
 import DropdownButton from "react-bootstrap/DropdownButton"
 import Dropdown from "react-bootstrap/Dropdown"
-import Dropdowner from "./DropDown";
-import './App.css';
 import FormGroup from 'react-bootstrap/FormGroup';
-import FormControl from 'react-bootstrap/FormControl';
-
-
-
 
 function onsave() {
   var school = document.getElementById("school").value;
@@ -29,13 +23,13 @@ function onsave() {
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = { color: '' };
   }
 
-  onPickColor(e){
-    console.log('[onPickColor]', this.inputEl )
+  onPickColor(e) {
+    console.log('[onPickColor]', this.inputEl)
     this.setState({ color: this.inputEl.value });
   }
   render() {
@@ -44,7 +38,7 @@ class App extends React.Component {
       <Container>
         <div className="mt-4 mb-4 mr-2 ml-2">
           <Form>
-            <h1 className="text-white">Template A1: Syllabus for Theory Courses (SAMPLE)</h1> <hr />
+            <h2 className="text-white"><b>Syllabus for Theory Courses</b></h2> <hr />
             <div>
               <Form.Group>
                 <Form.Label className="text-white">School</Form.Label>
@@ -68,13 +62,13 @@ class App extends React.Component {
             <div>
               <Form.Group controlId="Program">
                 <Form.Label className="text-white">Course Code</Form.Label>
-                <Form.Control placeholder="Password" id="course_code" />
+                <Form.Control placeholder="Code" id="course_code" />
               </Form.Group>
             </div>
             <div>
               <Form.Group controlId="Program">
                 <Form.Label className="text-white">Course Title</Form.Label>
-                <Form.Control placeholder="Password" id="course_title" />
+                <Form.Control placeholder="Title" id="course_title" />
               </Form.Group>
             </div>
             <div>
@@ -102,15 +96,15 @@ class App extends React.Component {
 
             <div align="center">
               <Form.Group
-              onChange={this.onPickColor.bind(this)}
-              inputRef={ el => this.inputEl=el }
-              componentClass="select" placeholder="select">
+                onChange={this.onPickColor.bind(this)}
+                inputRef={el => this.inputEl = el}
+                componentClass="select" placeholder="select">
 
                 <Form.Label className="text-white" id="courseStatus">Course Status</Form.Label>
-                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                  <option value = "Core">Core</option>
-                  <option value = "Elective">Elective</option>
-                  <option value = "Open Elective">Open Elective </option>
+                <DropdownButton id="dropdown-basic-button" title="Course Status">
+                  <Dropdown.Item href="#/action-1">Core</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Elective</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Open Elective </Dropdown.Item>
                 </DropdownButton>
 
               </Form.Group>
@@ -120,6 +114,21 @@ class App extends React.Component {
               <Form.Label className="text-white">Course Objective</Form.Label>
               <Form.Control as="textarea" aria-label="With textarea" />
             </FormGroup>
+
+            <Form.Group controlId="Program">
+              <Form.Label className="text-white">Course Outcomes</Form.Label>
+              <Form.Control as="textarea" type="autofill" placeholder="C01" id="C01" /> <br/>
+              <Form.Control as="textarea" type="autofill" placeholder="C02" id="C02" /> <br/>
+              <Form.Control as="textarea" type="autofill" placeholder="C03" id="C03" /> <br/>
+              <Form.Control as="textarea" type="autofill" placeholder="C04" id="C04" /> <br/>
+            </Form.Group>
+
+            <FormGroup>
+              <Form.Label className="text-white">Course Description</Form.Label>
+              <Form.Control as="textarea" placeholder = "Descrption of Course" aria-label="With textarea" />
+            </FormGroup>
+
+
 
             <div align="center" >
               <Button variant="primary" type="submit" onClick={onsave}>
