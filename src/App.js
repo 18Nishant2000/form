0319f919a5152197e2da78e5bc42881c5a833f40
Nishant Bansal <bansal.nishant18052000@gmail.com>
2019-11-03 16:43:46 +0530

    co1 avg working

--- src/App.js
+++ src/App.js
@@ -16,126 +16,127 @@ class App extends React.Component {
     parseInt(document.getElementById("CO5_1").value)+
     parseInt(document.getElementById("CO6_1").value))/6;
     window.alert(avg_co1)
+    document.getElementById("id_avg_co1").innerHTML=avg_co1;
 
-    var avg_co2=(parseInt(document.getElementById("CO1_2").value)+
-    parseInt(document.getElementById("CO2_2").value)+
-    parseInt(document.getElementById("CO3_2").value)+
-    parseInt(document.getElementById("CO4_2").value)+
-    parseInt(document.getElementById("CO5_2").value)+
-    parseInt(document.getElementById("CO6_2").value))/6;
-    window.alert(avg_co2)
+    // var avg_co2=(parseInt(document.getElementById("CO1_2").value)+
+    // parseInt(document.getElementById("CO2_2").value)+
+    // parseInt(document.getElementById("CO3_2").value)+
+    // parseInt(document.getElementById("CO4_2").value)+
+    // parseInt(document.getElementById("CO5_2").value)+
+    // parseInt(document.getElementById("CO6_2").value))/6;
+    // window.alert(avg_co2)
     
-    var avg_co3=(parseInt(document.getElementById("CO1_3").value)+
-    parseInt(document.getElementById("CO2_3").value)+
-    parseInt(document.getElementById("CO3_3").value)+
-    parseInt(document.getElementById("CO4_3").value)+
-    parseInt(document.getElementById("CO5_3").value)+
-    parseInt(document.getElementById("CO6_3").value))/6;
-    window.alert(avg_co3)
+    // var avg_co3=(parseInt(document.getElementById("CO1_3").value)+
+    // parseInt(document.getElementById("CO2_3").value)+
+    // parseInt(document.getElementById("CO3_3").value)+
+    // parseInt(document.getElementById("CO4_3").value)+
+    // parseInt(document.getElementById("CO5_3").value)+
+    // parseInt(document.getElementById("CO6_3").value))/6;
+    // window.alert(avg_co3)
 
-    var avg_co4=(parseInt(document.getElementById("CO1_4").value)+
-    parseInt(document.getElementById("CO2_4").value)+
-    parseInt(document.getElementById("CO3_4").value)+
-    parseInt(document.getElementById("CO4_4").value)+
-    parseInt(document.getElementById("CO5_4").value)+
-    parseInt(document.getElementById("CO6_4").value))/6;
-    window.alert(avg_co4)
+    // var avg_co4=(parseInt(document.getElementById("CO1_4").value)+
+    // parseInt(document.getElementById("CO2_4").value)+
+    // parseInt(document.getElementById("CO3_4").value)+
+    // parseInt(document.getElementById("CO4_4").value)+
+    // parseInt(document.getElementById("CO5_4").value)+
+    // parseInt(document.getElementById("CO6_4").value))/6;
+    // window.alert(avg_co4)
 
-    var avg_co5=(parseInt(document.getElementById("CO1_5").value)+
-    parseInt(document.getElementById("CO2_5").value)+
-    parseInt(document.getElementById("CO3_5").value)+
-    parseInt(document.getElementById("CO4_5").value)+
-    parseInt(document.getElementById("CO5_5").value)+
-    parseInt(document.getElementById("CO6_5").value))/6;
-    window.alert(avg_co5)
+    // var avg_co5=(parseInt(document.getElementById("CO1_5").value)+
+    // parseInt(document.getElementById("CO2_5").value)+
+    // parseInt(document.getElementById("CO3_5").value)+
+    // parseInt(document.getElementById("CO4_5").value)+
+    // parseInt(document.getElementById("CO5_5").value)+
+    // parseInt(document.getElementById("CO6_5").value))/6;
+    // window.alert(avg_co5)
 
-    var avg_co6=(parseInt(document.getElementById("CO1_6").value)+
-    parseInt(document.getElementById("CO2_6").value)+
-    parseInt(document.getElementById("CO3_6").value)+
-    parseInt(document.getElementById("CO4_6").value)+
-    parseInt(document.getElementById("CO5_6").value)+
-    parseInt(document.getElementById("CO6_6").value))/6;
-    window.alert(avg_co6)
+    // var avg_co6=(parseInt(document.getElementById("CO1_6").value)+
+    // parseInt(document.getElementById("CO2_6").value)+
+    // parseInt(document.getElementById("CO3_6").value)+
+    // parseInt(document.getElementById("CO4_6").value)+
+    // parseInt(document.getElementById("CO5_6").value)+
+    // parseInt(document.getElementById("CO6_6").value))/6;
+    // window.alert(avg_co6)
 
-    var avg_co7=(parseInt(document.getElementById("CO1_7").value)+
-    parseInt(document.getElementById("CO2_7").value)+
-    parseInt(document.getElementById("CO3_7").value)+
-    parseInt(document.getElementById("CO4_7").value)+
-    parseInt(document.getElementById("CO5_7").value)+
-    parseInt(document.getElementById("CO6_7").value))/6;
-    window.alert(avg_co7)
+    // var avg_co7=(parseInt(document.getElementById("CO1_7").value)+
+    // parseInt(document.getElementById("CO2_7").value)+
+    // parseInt(document.getElementById("CO3_7").value)+
+    // parseInt(document.getElementById("CO4_7").value)+
+    // parseInt(document.getElementById("CO5_7").value)+
+    // parseInt(document.getElementById("CO6_7").value))/6;
+    // window.alert(avg_co7)
 
-    var avg_co8=(parseInt(document.getElementById("CO1_8").value)+
-    parseInt(document.getElementById("CO2_8").value)+
-    parseInt(document.getElementById("CO3_8").value)+
-    parseInt(document.getElementById("CO4_8").value)+
-    parseInt(document.getElementById("CO5_8").value)+
-    parseInt(document.getElementById("CO6_8").value))/6;
-    window.alert(avg_co8)
+    // var avg_co8=(parseInt(document.getElementById("CO1_8").value)+
+    // parseInt(document.getElementById("CO2_8").value)+
+    // parseInt(document.getElementById("CO3_8").value)+
+    // parseInt(document.getElementById("CO4_8").value)+
+    // parseInt(document.getElementById("CO5_8").value)+
+    // parseInt(document.getElementById("CO6_8").value))/6;
+    // window.alert(avg_co8)
 
-    var avg_co9=(parseInt(document.getElementById("CO1_9").value)+
-    parseInt(document.getElementById("CO2_9").value)+
-    parseInt(document.getElementById("CO3_9").value)+
-    parseInt(document.getElementById("CO4_9").value)+
-    parseInt(document.getElementById("CO5_9").value)+
-    parseInt(document.getElementById("CO6_9").value))/6;
-    window.alert(avg_co9)
+    // var avg_co9=(parseInt(document.getElementById("CO1_9").value)+
+    // parseInt(document.getElementById("CO2_9").value)+
+    // parseInt(document.getElementById("CO3_9").value)+
+    // parseInt(document.getElementById("CO4_9").value)+
+    // parseInt(document.getElementById("CO5_9").value)+
+    // parseInt(document.getElementById("CO6_9").value))/6;
+    // window.alert(avg_co9)
 
-    var avg_co10=(parseInt(document.getElementById("CO1_10").value)+
-    parseInt(document.getElementById("CO2_10").value)+
-    parseInt(document.getElementById("CO3_10").value)+
-    parseInt(document.getElementById("CO4_10").value)+
-    parseInt(document.getElementById("CO5_10").value)+
-    parseInt(document.getElementById("CO6_10").value))/6;
-    window.alert(avg_co10)
+    // var avg_co10=(parseInt(document.getElementById("CO1_10").value)+
+    // parseInt(document.getElementById("CO2_10").value)+
+    // parseInt(document.getElementById("CO3_10").value)+
+    // parseInt(document.getElementById("CO4_10").value)+
+    // parseInt(document.getElementById("CO5_10").value)+
+    // parseInt(document.getElementById("CO6_10").value))/6;
+    // window.alert(avg_co10)
 
-    var avg_co11=(parseInt(document.getElementById("CO1_11").value)+
-    parseInt(document.getElementById("CO2_11").value)+
-    parseInt(document.getElementById("CO3_11").value)+
-    parseInt(document.getElementById("CO4_11").value)+
-    parseInt(document.getElementById("CO5_11").value)+
-    parseInt(document.getElementById("CO6_11").value))/6;
-    window.alert(avg_co11)
+    // var avg_co11=(parseInt(document.getElementById("CO1_11").value)+
+    // parseInt(document.getElementById("CO2_11").value)+
+    // parseInt(document.getElementById("CO3_11").value)+
+    // parseInt(document.getElementById("CO4_11").value)+
+    // parseInt(document.getElementById("CO5_11").value)+
+    // parseInt(document.getElementById("CO6_11").value))/6;
+    // window.alert(avg_co11)
 
-    var avg_co12=(parseInt(document.getElementById("CO1_12").value)+
-    parseInt(document.getElementById("CO2_12").value)+
-    parseInt(document.getElementById("CO3_12").value)+
-    parseInt(document.getElementById("CO4_12").value)+
-    parseInt(document.getElementById("CO5_12").value)+
-    parseInt(document.getElementById("CO6_12").value))/6;
-    window.alert(avg_co12)
+    // var avg_co12=(parseInt(document.getElementById("CO1_12").value)+
+    // parseInt(document.getElementById("CO2_12").value)+
+    // parseInt(document.getElementById("CO3_12").value)+
+    // parseInt(document.getElementById("CO4_12").value)+
+    // parseInt(document.getElementById("CO5_12").value)+
+    // parseInt(document.getElementById("CO6_12").value))/6;
+    // window.alert(avg_co12)
 
-    var avg_co13=(parseInt(document.getElementById("CO1_13").value)+
-    parseInt(document.getElementById("CO2_13").value)+
-    parseInt(document.getElementById("CO3_13").value)+
-    parseInt(document.getElementById("CO4_13").value)+
-    parseInt(document.getElementById("CO5_13").value)+
-    parseInt(document.getElementById("CO6_13").value))/6;
-    window.alert(avg_co13)
+    // var avg_co13=(parseInt(document.getElementById("CO1_13").value)+
+    // parseInt(document.getElementById("CO2_13").value)+
+    // parseInt(document.getElementById("CO3_13").value)+
+    // parseInt(document.getElementById("CO4_13").value)+
+    // parseInt(document.getElementById("CO5_13").value)+
+    // parseInt(document.getElementById("CO6_13").value))/6;
+    // window.alert(avg_co13)
 
-    var avg_co14=(parseInt(document.getElementById("CO1_14").value)+
-    parseInt(document.getElementById("CO2_14").value)+
-    parseInt(document.getElementById("CO3_14").value)+
-    parseInt(document.getElementById("CO4_14").value)+
-    parseInt(document.getElementById("CO5_14").value)+
-    parseInt(document.getElementById("CO6_14").value))/6;
-    window.alert(avg_co14)
+    // var avg_co14=(parseInt(document.getElementById("CO1_14").value)+
+    // parseInt(document.getElementById("CO2_14").value)+
+    // parseInt(document.getElementById("CO3_14").value)+
+    // parseInt(document.getElementById("CO4_14").value)+
+    // parseInt(document.getElementById("CO5_14").value)+
+    // parseInt(document.getElementById("CO6_14").value))/6;
+    // window.alert(avg_co14)
 
-    var avg_co15=(parseInt(document.getElementById("CO1_15").value)+
-    parseInt(document.getElementById("CO2_15").value)+
-    parseInt(document.getElementById("CO3_15").value)+
-    parseInt(document.getElementById("CO4_15").value)+
-    parseInt(document.getElementById("CO5_15").value)+
-    parseInt(document.getElementById("CO6_15").value))/6;
-    window.alert(avg_co15)
+    // var avg_co15=(parseInt(document.getElementById("CO1_15").value)+
+    // parseInt(document.getElementById("CO2_15").value)+
+    // parseInt(document.getElementById("CO3_15").value)+
+    // parseInt(document.getElementById("CO4_15").value)+
+    // parseInt(document.getElementById("CO5_15").value)+
+    // parseInt(document.getElementById("CO6_15").value))/6;
+    // window.alert(avg_co15)
 
-    var avg_co16=(parseInt(document.getElementById("CO1_16").value)+
-    parseInt(document.getElementById("CO2_16").value)+
-    parseInt(document.getElementById("CO3_16").value)+
-    parseInt(document.getElementById("CO4_16").value)+
-    parseInt(document.getElementById("CO5_16").value)+
-    parseInt(document.getElementById("CO6_16").value))/6;
-    window.alert(avg_co16)
+    // var avg_co16=(parseInt(document.getElementById("CO1_16").value)+
+    // parseInt(document.getElementById("CO2_16").value)+
+    // parseInt(document.getElementById("CO3_16").value)+
+    // parseInt(document.getElementById("CO4_16").value)+
+    // parseInt(document.getElementById("CO5_16").value)+
+    // parseInt(document.getElementById("CO6_16").value))/6;
+    // window.alert(avg_co16)
   }
   addUser = e => {
     e.preventDefault();
@@ -758,7 +759,7 @@ class App extends React.Component {
               <Form.Label className="text-white"><h3><b>Average of non-zeros entry in following table. </b></h3> </Form.Label>
               <Row>
                 <Col>
-                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
+                  <p align="center" className="text-white" plaintext id="id_avg_co1" >0</p>
                 </Col>
                 <Col>
                   <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
@@ -802,16 +803,16 @@ class App extends React.Component {
               <br />
               <Row>
                 <Col>
-                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="PSO 1" />
+                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
                 </Col>
                 <Col>
-                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="PSO 2" />
+                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
                 </Col>
                 <Col>
-                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="PSO 3" />
+                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
                 </Col>
                 <Col>
-                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="PSO 4" />
+                  <Form.Control align="center" className="text-white" plaintext readOnly defaultValue="0" />
                 </Col>
               </Row>
             </FormGroup>
